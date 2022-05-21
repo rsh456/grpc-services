febcd6e50607181e4a20df5fb2039ed554314fa6
rsh456 <rsh456@hotmail.com>
2022-05-20 19:18:51 -0500

    add rocket service and unit test

--- internal/rocket/rocket.go
+++ internal/rocket/rocket.go
@@ -1,4 +1,10 @@
+//go:generate mockgen -destination=rocket_mocks_test.go -package=rocket github.com/rsh456/grpc-services/internal/rocket Store
+
 package rocket
+
+import (
+	"context"
+)
 
 // Rocket - should contain the definition of our
 // rocket
@@ -7,4 +13,53 @@ type Rocket struct {
 	Name    string
 	Type    string
 	Flights int
+}
+
+// Store - defines the interface we expect
+// our database implementation to follow
+type Store interface {
+	GetRocketByID(id string) (Rocket, error)
+	InsertRocket(rkt Rocket) (Rocket, error)
+	DeleteRocket(id string) error
+}
+
+// Service - our rocket server, responsible for
+//updating the rocket
+type Service struct {
+	Store Store
+}
+
+// New - returns a new instance for our rocket service
+func New(store Store) Service {
+	return Service{
+		Store: store,
+	}
+}
+
+// ctx allow us for instance cancel an incoming request
+
+// GetRocketByID - retrieves a rocket based on the ID from the store
+func (s Service) GetRocketByID(ctx context.Context, id string) (Rocket, error) {
+	rkt, err := s.Store.GetRocketByID(id)
+	if err != nil {
+		return Rocket{}, err
+	}
+	return rkt, nil
+}
+
+// InsertRocket - inserts a new rocket into the store
+func (s Service) InsertRocket(ctx context.Context, rkt Rocket) (Rocket, error) {
+	rkt, err := s.Store.InsertRocket(rkt)
+	if err != nil {
+		return Rocket{}, err
+	}
+	return rkt, nil
+}
+
+func (s Service) DeleteRocket(ctx context.Context, id string) error {
+	err := s.Store.DeleteRocket(id)
+	if err != nil {
+		return err
+	}
+	return nil
 }
